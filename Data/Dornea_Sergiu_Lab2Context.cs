9f893b5372face4ce81e1777d39a9ebc8160392a
SergiuDornea <88648596+SergiuDornea@users.noreply.github.com>
2023-10-19 11:31:44 +0300

    Add project files.

---
+++ Data/Dornea_Sergiu_Lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Dornea_Sergiu_Lab2.Models;
+
+namespace Dornea_Sergiu_Lab2.Data
+{
+    public class Dornea_Sergiu_Lab2Context : DbContext
+    {
+        public Dornea_Sergiu_Lab2Context (DbContextOptions<Dornea_Sergiu_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Dornea_Sergiu_Lab2.Models.Book> Book { get; set; } = default!;
+    }
+}
