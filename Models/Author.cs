f474f82816c96de415ec626dbcc6d3d83b7fa2c6
SergiuDornea <88648596+SergiuDornea@users.noreply.github.com>
2023-10-27 10:40:12 +0300

    lab2 - complete

--- Models/Author.cs
+++ Models/Author.cs
@@ -7,6 +7,7 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
+        public string AuthorName => $"{FirstName} {LastName}";
         public ICollection<Book>? Books { get; set; } // nav property for Book
 
 
--- Pages/Books/Create.cshtml.cs
+++ Pages/Books/Create.cshtml.cs
@@ -22,7 +22,7 @@ namespace Dornea_Sergiu_Lab2.Pages.Books
         public IActionResult OnGet()
         {
             ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID","PublisherName");
-            ViewData["AuthorID"] = new SelectList(_context.Set<Author>(), "ID", "FirstName");
+            ViewData["AuthorID"] = new SelectList(_context.Set<Author>(), "ID", "AuthorName");
            
             return Page();
         }
