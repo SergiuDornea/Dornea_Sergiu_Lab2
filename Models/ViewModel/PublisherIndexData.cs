449769087ea00a64518711108aeca3eecc83c027
SergiuDornea <88648596+SergiuDornea@users.noreply.github.com>
2023-11-03 09:14:58 +0200

    up to 6

---
+++ Models/ViewModel/PublisherIndexData.cs
@@ -0,0 +1,8 @@
+﻿namespace Dornea_Sergiu_Lab2.Models.ViewModel
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
