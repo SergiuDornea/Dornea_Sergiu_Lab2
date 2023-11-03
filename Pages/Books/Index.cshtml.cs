2e9c9608887327cfaa489cb8e065dbb6d72d31b3
SergiuDornea <88648596+SergiuDornea@users.noreply.github.com>
2023-11-03 09:56:33 +0200

    up to sarcina lab

--- Pages/Books/Index.cshtml.cs
+++ Pages/Books/Index.cshtml.cs
@@ -19,7 +19,7 @@ namespace Dornea_Sergiu_Lab2.Pages.Books
             _context = context;
         }
 
-        public IList<Book> Book { get;set; } = default!;
+        public IList<Book> Book { get; set; } = default!;
 
         public BookData BookD { get; set; }
         public int BookID { get; set; }
@@ -27,13 +27,16 @@ namespace Dornea_Sergiu_Lab2.Pages.Books
 
         public string TitleSort { get; set; }
         public string AuthorSort { get; set; }
-        public async Task OnGetAsync(int? id, int? categoryID, string sortOrder)
+        public string CurrentFilter { get; set; }
+
+        public async Task OnGetAsync(int? id, int? categoryID, string sortOrder, string searchString)
         {
             BookD = new BookData();
 
             TitleSort = String.IsNullOrEmpty(sortOrder) ? "title_desc" : "";
             AuthorSort = sortOrder == "author" ? "author_desc" : "author";
 
+            CurrentFilter = searchString;
 
             BookD.Books = await _context.Book
             .Include(b => b.Publisher)
@@ -43,31 +46,39 @@ namespace Dornea_Sergiu_Lab2.Pages.Books
             .AsNoTracking()
             .OrderBy(b => b.Title)
             .ToListAsync();
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                BookD.Books = BookD.Books.Where(s => s.Author.AuthorName.Contains(searchString)
+               || s.Title.Contains(searchString));
+            }
+
             if (id != null)
-            {
-                BookID = id.Value;
-                Book book = BookD.Books
-                .Where(i => i.ID == id.Value).Single();
-                BookD.Categories = book.BookCategories.Select(s => s.Category);
-            }
-            switch (sortOrder)
-            {
-                case "title_desc":
-                    BookD.Books = BookD.Books.OrderByDescending(s =>
-                   s.Title);
-                    break;
-                case "author_desc":
-                    BookD.Books = BookD.Books.OrderByDescending(s =>
-                   s.Author.AuthorName);
-                    break;
-                case "author":
-                    BookD.Books = BookD.Books.OrderBy(s =>
-                   s.Author.AuthorName);
-                    break;
-                default:
-                    BookD.Books = BookD.Books.OrderBy(s => s.Title);
-                    break;
-            }
+                {
+                    BookID = id.Value;
+                    Book book = BookD.Books
+                    .Where(i => i.ID == id.Value).Single();
+                    BookD.Categories = book.BookCategories.Select(s => s.Category);
+                }
+                switch (sortOrder)
+                {
+                    case "title_desc":
+                        BookD.Books = BookD.Books.OrderByDescending(s =>
+                       s.Title);
+                        break;
+                    case "author_desc":
+                        BookD.Books = BookD.Books.OrderByDescending(s =>
+                       s.Author.AuthorName);
+                        break;
+                    case "author":
+                        BookD.Books = BookD.Books.OrderBy(s =>
+                       s.Author.AuthorName);
+                        break;
+                    default:
+                        BookD.Books = BookD.Books.OrderBy(s => s.Title);
+                        break;
+                }
         }
     }
 }
+
