66ae567cf7f62e88b0f53c3778d1b69fe80a8972
SergiuDornea <88648596+SergiuDornea@users.noreply.github.com>
2023-11-03 09:32:17 +0200

    up to 11

--- Pages/Books/Index.cshtml.cs
+++ Pages/Books/Index.cshtml.cs
@@ -24,9 +24,16 @@ namespace Dornea_Sergiu_Lab2.Pages.Books
         public BookData BookD { get; set; }
         public int BookID { get; set; }
         public int CategoryID { get; set; }
-        public async Task OnGetAsync(int? id, int? categoryID)
+
+        public string TitleSort { get; set; }
+        public string AuthorSort { get; set; }
+        public async Task OnGetAsync(int? id, int? categoryID, string sortOrder)
         {
             BookD = new BookData();
+
+            TitleSort = String.IsNullOrEmpty(sortOrder) ? "title_desc" : "";
+            AuthorSort = sortOrder == "author" ? "author_desc" : "author";
+
 
             BookD.Books = await _context.Book
             .Include(b => b.Publisher)
@@ -42,6 +49,24 @@ namespace Dornea_Sergiu_Lab2.Pages.Books
                 Book book = BookD.Books
                 .Where(i => i.ID == id.Value).Single();
                 BookD.Categories = book.BookCategories.Select(s => s.Category);
+            }
+            switch (sortOrder)
+            {
+                case "title_desc":
+                    BookD.Books = BookD.Books.OrderByDescending(s =>
+                   s.Title);
+                    break;
+                case "author_desc":
+                    BookD.Books = BookD.Books.OrderByDescending(s =>
+                   s.Author.AuthorName);
+                    break;
+                case "author":
+                    BookD.Books = BookD.Books.OrderBy(s =>
+                   s.Author.AuthorName);
+                    break;
+                default:
+                    BookD.Books = BookD.Books.OrderBy(s => s.Title);
+                    break;
             }
         }
     }
