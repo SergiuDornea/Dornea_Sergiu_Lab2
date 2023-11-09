58440738ae7586a3bb66661a491dca75f7e05e07
SergiuDornea <88648596+SergiuDornea@users.noreply.github.com>
2023-11-09 11:53:43 +0200

    up to tema

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
+using Dornea_Sergiu_Lab2.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -29,13 +30,16 @@ namespace Dornea_Sergiu_Lab2.Areas.Identity.Pages.Account
         private readonly IUserEmailStore<IdentityUser> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
+        private readonly Dornea_Sergiu_Lab2.Data.Dornea_Sergiu_Lab2Context
+_context;
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
             IUserStore<IdentityUser> userStore,
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            Dornea_Sergiu_Lab2.Data.Dornea_Sergiu_Lab2Context context)
         {
             _userManager = userManager;
             _userStore = userStore;
@@ -43,7 +47,11 @@ namespace Dornea_Sergiu_Lab2.Areas.Identity.Pages.Account
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            _context = context;
         }
+
+        [BindProperty]
+        public Member Member { get; set; }
 
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -109,48 +117,60 @@ namespace Dornea_Sergiu_Lab2.Areas.Identity.Pages.Account
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
             returnUrl ??= Url.Content("~/");
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-            if (ModelState.IsValid)
+            ExternalLogins = (await
+           _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
+            var user = CreateUser();
+            await _userStore.SetUserNameAsync(user, Input.Email,
+           CancellationToken.None);
+            await _emailStore.SetEmailAsync(user, Input.Email,
+           CancellationToken.None);
+            var result = await _userManager.CreateAsync(user,
+           Input.Password);
+            Member.Email = Input.Email;
+            _context.Member.Add(Member);
+            await _context.SaveChangesAsync();
+            if (result.Succeeded)
             {
-                var user = CreateUser();
+                _logger.LogInformation("User created a new account with password.");
+               
+                var userId = await _userManager.GetUserIdAsync(user);
+                var code = await
+               _userManager.GenerateEmailConfirmationTokenAsync(user);
+                code =
+               WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                var callbackUrl = Url.Page(
+                "/Account/ConfirmEmail",
+               pageHandler: null,
+               values: new
+               {
+                   area = "Identity",
+                   userId = userId,
+                   code = code,
+                   returnUrl = returnUrl
+               },
+                protocol: Request.Scheme);
+                await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+               $"Please confirm your account by <ahref = '{HtmlEncoder.Default.Encode(callbackUrl)}' > clicking here </ a >.");
+           
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
-                await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
-                var result = await _userManager.CreateAsync(user, Input.Password);
-
-                if (result.Succeeded)
+ if
+(_userManager.Options.SignIn.RequireConfirmedAccount)
                 {
-                    _logger.LogInformation("User created a new account with password.");
-
-                    var userId = await _userManager.GetUserIdAsync(user);
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
-                        protocol: Request.Scheme);
-
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    return RedirectToPage("RegisterConfirmation", new
                     {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                    }
-                    else
-                    {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
-                    }
+                        email = Input.Email,
+                        returnUrl = returnUrl
+                    });
                 }
-                foreach (var error in result.Errors)
+                else
                 {
-                    ModelState.AddModelError(string.Empty, error.Description);
+                    await _signInManager.SignInAsync(user,
+                   isPersistent: false);
+                    return LocalRedirect(returnUrl);
                 }
+
             }
-
-            // If we got this far, something failed, redisplay form
             return Page();
         }
 
