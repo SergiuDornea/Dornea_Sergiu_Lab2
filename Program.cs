0b7b6ec909a006f348aedb1aa6ca03b738d06981
SergiuDornea <88648596+SergiuDornea@users.noreply.github.com>
2023-11-14 15:22:20 +0200

    up to 6

--- Program.cs
+++ Program.cs
@@ -5,7 +5,13 @@ using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AuthorizeFolder("/Books");
+    options.Conventions.AllowAnonymousToPage("/Books/Index");
+    options.Conventions.AllowAnonymousToPage("/Books/Details");
+
+});
 builder.Services.AddDbContext<Dornea_Sergiu_Lab2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Dornea_Sergiu_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Dornea_Sergiu_Lab2Context' not found.")));
 
@@ -15,6 +21,7 @@ builder.Services.AddDbContext<LibraryIdentityContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("Dornea_Sergiu_Lab2Context") ?? throw new InvalidOperationException("Connectionstring 'Nume_Pren_Lab2Context' not found.")));
 builder.Services.AddDefaultIdentity<IdentityUser>(options =>
 options.SignIn.RequireConfirmedAccount = true)
+ .AddRoles<IdentityRole>()
  .AddEntityFrameworkStores<LibraryIdentityContext>();
 
 var app = builder.Build();
