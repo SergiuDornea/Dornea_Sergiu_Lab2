38a842e0bbce03f959fe4c6193018f2b4a5e58ca
SergiuDornea <88648596+SergiuDornea@users.noreply.github.com>
2023-11-15 14:13:39 +0200

    up to tema - admin account to de🐜

--- Models/Book.cs
+++ Models/Book.cs
@@ -10,11 +10,11 @@ namespace Dornea_Sergiu_Lab2.Models
 
         [Display(Name = "Book Title")]
         public string Title { get; set; }
-        
+
         public int? AuthorID { get; set; }
         public Author? Author { get; set; } // nav property for author
 
-        [Column(TypeName = "decimal(6, 2)")]
+        [Column(TypeName = "decimal(6, 2)")] [Range(0.01, 500)]
         public decimal Price { get; set; }
 
         [DataType(DataType.Date)]
--- Models/Member.cs
+++ Models/Member.cs
@@ -7,7 +7,6 @@ namespace Dornea_Sergiu_Lab2.Models
         public int ID { get; set; }
         [RegularExpression(@"^[A-Z]+[a-zA-Z\s-]*$", ErrorMessage =
 "Prenumele trebuie sa inceapa cu majuscula (ex. Ana sau Ana Maria sau AnaMaria")]
-        [
         [StringLength(30, MinimumLength = 3)]
         public string? FirstName { get; set; }
 
@@ -18,6 +17,8 @@ namespace Dornea_Sergiu_Lab2.Models
         [StringLength(70)]
         public string? Adress { get; set; }
         public string Email { get; set; }
+
+        [RegularExpression(@"^\(?([0-9]{4})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{3})$", ErrorMessage = "Telefonul trebuie sa fie de forma '0722-123-123' sau '0722.123.123' sau '0722 123 123'")]
         public string? Phone { get; set; }
         [Display(Name = "Full Name")]
         public string? FullName
